eb765e98a09650b306db4b134a71fcfa478923d7
Chaeyun Yang <ycy6685@naver.com>
2016-06-20 20:55:48 +0900

    #2 Make Tab

---
+++ app/src/main/java/kr/hs/emirim/ycy6685/kingofring/Fragment2.java
@@ -0,0 +1,20 @@
+package kr.hs.emirim.ycy6685.kingofring;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by appcreator04 on 2016. 6. 20..
+ */
+public class Fragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment2, container, false);
+
+        return rootView;
+    }
+}
